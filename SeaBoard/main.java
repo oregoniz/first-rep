f402bead6b73e2a3468ed33d9a6eab00a1a68a5d
regoniz <olga.regoniz@gmail.com>
2023-01-22 12:51:08 +0300

    210123

--- SeaBoard/main.java
+++ SeaBoard/main.java
@@ -3,7 +3,7 @@ package SeaBoard;
 public class main {
     public static void main(String[] args) {
         SeaBoard game = new SeaBoard();
-        /*game.shoot(0, 2, "h");
+        game.shoot(0, 2, "h");
         game.shoot(8, 2, "d");
         game.printField(game.getField());
         game.shoot(9, 6, "h");
@@ -14,7 +14,7 @@ public class main {
         game.shoot(1, 7, "h");
         game.shoot(2, 7, "h");
         game.shoot(3, 7, "d");
-        game.printField(game.getField());*/
+        game.printField(game.getField());
         game.shoot(8, 9, "d");
         game.shoot(9, 9, "d");
         // game.shoot(1, 2, "d");
